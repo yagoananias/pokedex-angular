45fbb95b15a087e0aa44e173cdac25743bba3df6
Yago Ananias <yago_souza92@yahoo.com.br>
2021-07-01 06:13:28 -0300

    Criando o data.service

---
+++ pokedex-angular/src/app/services/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  //Get Pokemons
+
+  getPokemons() {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon?limit=10')
+  }
+}
